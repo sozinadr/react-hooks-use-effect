f13d770162d25753ed4c1a64c000c40d7a4f6669
sozi <sozyarnadir@gmail.com>
2022-10-24 22:52:09 +0300

    Assignment Completed

--- src/DogPics.js
+++ src/DogPics.js
@@ -1,29 +1,38 @@
 import React, { useState, useEffect } from "react";
 
-// WARNING: this useEffect will run in an infinite loop!
-// to fix, pass an empty array as the second argument for useEffect
-function DogPics() {
-  const [images, setImages] = useState([]);
+function App() {
+    const [count, setCount] = useState(0);
+    const [text, setText] = useState("");
 
-  useEffect(() => {
-    console.log("useEffect");
-    fetch("https://dog.ceo/api/breeds/image/random/3")
-      .then((r) => r.json())
-      .then((data) => {
-        console.log("setState");
-        setImages(data.message);
-      });
-  });
+    useEffect(() => {
+        document.title = text;
+    }, [text]);
 
-  console.log("render");
+    useEffect(() => {
 
-  return (
-    <div>
-      {images.map((image) => (
-        <img src={image} key={image} />
-      ))}
-    </div>
-  );
+        setTimeout(() => setCount(0), 5000);
+    }, []);
+
+    console.log("Component rendering");
+
+
+    return ( <
+        div >
+        <
+        button onClick = {
+            () => setCount((count) => count + 1)
+        } >
+        I 've been clicked {count} times < /
+        button > <
+        input type = "text"
+        placeholder = "Type away..."
+        value = { text }
+        onChange = {
+            (e) => setText(e.target.value)
+        }
+        /> < /
+        div >
+    );
 }
 
-export default DogPics;
+export default App;
